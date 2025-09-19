6a5cf994281036447da66c854fd1279d38be3c2d
yerinJi <yerin6428@gmail.com>
2025-09-19 11:17:45 +0900

    프로젝트 생성

---
+++ src/main/java/com/multi/kakaomapminipjt/KakaoMapMiniPjtApplication.java
@@ -0,0 +1,13 @@
+package com.multi.kakaomapminipjt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaoMapMiniPjtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KakaoMapMiniPjtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/multi/kakaomapminipjt/KakaoMapMiniPjtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multi.kakaomapminipjt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaoMapMiniPjtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
