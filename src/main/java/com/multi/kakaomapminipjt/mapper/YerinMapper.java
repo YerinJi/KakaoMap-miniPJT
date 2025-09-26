48d49136ddb723c43d4c9e27a988cfbf43874d56
YUK <yukkwae@naver.com>
2025-09-26 10:23:27 +0900

    최종 99.99%

--- src/main/java/com/multi/kakaomapminipjt/mapper/YerinMapper.java
+++ src/main/java/com/multi/kakaomapminipjt/mapper/YerinMapper.java
@@ -1,21 +1,21 @@
-//package com.multi.kakaomapminipjt.mapper;
-//
-//import com.multi.kakaomapminipjt.Pagination;
-//import com.multi.kakaomapminipjt.dto.YerinTravel;
-//import org.apache.ibatis.annotations.Mapper;
-//import org.apache.ibatis.annotations.Param;
-//import org.apache.ibatis.annotations.Select;
-//
-//import java.util.List;
-//
-//@Mapper
-//public interface YerinMapper {
-//    @Select("SELECT * FROM travel")
-//    List<YerinTravel> getYerinTravels();
-//
-//    @Select("SELECT COUNT(*) FROM travel WHERE title LIKE CONCAT('%', #{keyword}, '%') OR address LIKE CONCAT('%', #{keyword}, '%')")
-//    int countByKeyword(@Param("keyword") String keyword);
-//
-//    @Select("SELECT * FROM travel WHERE title LIKE CONCAT('%', #{keyword}, '%') OR address LIKE CONCAT('%', #{keyword}, '%') LIMIT #{pagination.size} OFFSET #{pagination.offset}")
-//    List<YerinTravel> searchByKeyword(@Param("keyword") String keyword, @Param("pagination") Pagination pagination);
-//}
+package com.multi.kakaomapminipjt.mapper;
+
+import com.multi.kakaomapminipjt.Pagination;
+import com.multi.kakaomapminipjt.dto.YerinTravel;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface YerinMapper {
+    @Select("SELECT * FROM travel")
+    List<YerinTravel> getYerinTravels();
+
+    @Select("SELECT COUNT(*) FROM travel WHERE title LIKE CONCAT('%', #{keyword}, '%') OR address LIKE CONCAT('%', #{keyword}, '%')")
+    int countByKeyword(@Param("keyword") String keyword);
+
+    @Select("SELECT * FROM travel WHERE title LIKE CONCAT('%', #{keyword}, '%') OR address LIKE CONCAT('%', #{keyword}, '%') LIMIT #{pagination.size} OFFSET #{pagination.offset}")
+    List<YerinTravel> searchByKeyword(@Param("keyword") String keyword, @Param("pagination") Pagination pagination);
+}
